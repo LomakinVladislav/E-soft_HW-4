894bb1fd8d12959589c29459fa489389f2fd4920
Vladislav <winr2005@gmail.com>
2025-06-07 20:13:26 +0500

    Инициализация проекта, добавлен и стилизован компонент карточки книги

---
+++ e-soft-hw-4/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import BookCard from './components/BookCard'
+
+function App() {
+  
+
+  return (
+    <>
+      <BookCard />
+      <BookCard />
+    </>
+  )
+}
+
+export default App
---
+++ e-soft-hw-4/src/components/BookCard.jsx
@@ -0,0 +1,32 @@
+import styles from '/src/styles/BookCard.module.css'
+
+
+function BookCard() {
+    return (
+        <div className={styles.bookCard}>
+            <div className={styles.container}>
+                <div>
+                    <img 
+                        src="/src/assets/book.png" 
+                        alt="Обложка"  
+                        width={170}
+                    />
+                </div>
+                <h1>Название</h1>
+                <div>
+                    <h2>Автор</h2>
+                    <h3>Год издания</h3>
+                </div>
+            </div>
+            <button className={styles.favouriteButton}>
+                <img 
+                    src="/src/assets/icon-favourites_black.png" 
+                    alt="В избранное" 
+                    width={30}
+                />
+            </button>
+        </div>
+    );
+}
+
+export default BookCard
